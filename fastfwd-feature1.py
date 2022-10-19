ea90ce460182644f7e88722c1493402d9f35cdc6
K.Inozemtseva <inozem.k0@gmail.com>
2022-10-19 16:43:19 +0300

    Add fastwd example

---
+++ fastfwd-feature1.py
@@ -0,0 +1,6 @@
+### TODO tell the world how awesome is your feature
+def even_more_awesomeness():
+    print("I'm awesome feature example!")
+
+if __name__ == "__main__":
+    even_more_awesomeness()
