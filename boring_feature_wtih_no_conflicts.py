b4eadbea31d5390e269624dd0dac32583025ad55
K.Inozemtseva <inozem.k0@gmail.com>
2022-10-19 17:32:29 +0300

    Change master to have conflict

--- boring_feature_wtih_no_conflicts.py
+++ boring_feature_wtih_no_conflicts.py
@@ -1,6 +1,7 @@
 ### TODO tell the world how boring is your feature
 def even_more_boringness():
     print("I'm boring feature example!")
+    print("This is not boring enough")
 
 if __name__ == "__main__":
     even_more_boringness()
